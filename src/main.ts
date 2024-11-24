eebf8ee2a7fbb8e463387139cc6d5a0c393b69a0
arrancareye <arrancareye@gmail.com>
2024-11-24 14:04:25 +0100

    add api prefix, adjust gitignore

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import { JwtAuthGuard } from './auth/jwt-auth.guard';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
   app.useGlobalGuards(new JwtAuthGuard());
+  app.setGlobalPrefix('api');
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
